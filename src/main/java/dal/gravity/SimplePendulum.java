38a3192fc8f619e6ff417e60eaca8377f934f7ad
mr73er <mr73er@gmail.com>
2017-04-01 21:11:56 -0300

    remove abstractEarthPendulum class, given it is a sub-class of AbstractPendulum and is the super class of regularPendulum&SimplePendulum, the only thing this class do is to define the gravity, so i define the gravity in both rP class and sP class and make a change of the constructor, still get the same result.

--- src/main/java/dal/gravity/SimplePendulum.java
+++ src/main/java/dal/gravity/SimplePendulum.java
@@ -3,9 +3,11 @@ package dal.gravity;
 /**
  * Represents pendulums exhibiting (approximately) simple harmonic motion
  */
-public class SimplePendulum extends AbstractEarthPendulum {
+public class SimplePendulum extends AbstractPendulum {
 
     private double angularFrequency, periodOfMotion;
+    
+    public static final double GRAVITY = 9.80665;
 
     /**
      * Creates a new Pendulum instance using
@@ -14,7 +16,7 @@ public class SimplePendulum extends AbstractEarthPendulum {
      * inTheta0: angular displacement at t=0 (0<=theta0<=pi/6)
      */
     public SimplePendulum (double inLength, double inMass, double inTheta0) {
-	super (inLength, inMass, inTheta0);
+	super (inLength, inMass, inTheta0, GRAVITY);
 	angularFrequency = Math.sqrt (this.getGravitationalField () / this.getStringLength ());
 	periodOfMotion = 2 * Math.PI 
 	    * Math.sqrt (this.getStringLength () / this.getGravitationalField ());
